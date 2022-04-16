f832f206b23bd076fc1151670d41e12f07060f98
Gerolineto <gabrielgerolineto@gmail.com>
2022-04-16 03:10:52 -0300

    First frame

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
+
+
+function loop() {
+    contexto.drawImage(
+        sprites,
+        0, 0, //sprite x e sprite y
+        33, 24, //tamanho do recorte na sprite
+        10, 50,
+        33, 24,
+    );
+
+    requestAnimationFrame(loop);
+}
+
+loop();
+
+
