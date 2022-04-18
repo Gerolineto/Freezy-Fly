e0a7b45457cfbc08d8e8170bf389b45b3c8494ac
Gerolineto <gabrielgerolineto@gmail.com>
2022-04-17 22:47:25 -0300

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('[DevSoutinho] Flappy Bird');
 console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
 
 const sprites = new Image();
-sprites.src = './sprites.png';
+sprites.src = './spritesf.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
