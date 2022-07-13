8862b269cbcc13c2dbac538aacef7e9db1619b21
Gerolineto <gabrielgerolineto@gmail.com>
2022-07-12 22:08:42 -0300

    definição do layout

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('[DevSoutinho] Flappy Bird');
 console.log('Inscreva-se no canal :D https://www.youtube.com/channel/UCzR2u5RWXWjUh7CwLSvbitA');
 
 const sprites = new Image();
-sprites.src = './spritesf.png';
+sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
@@ -12,7 +12,7 @@ function loop() {
     contexto.drawImage(
         sprites,
         0, 0, //sprite x e sprite y
-        33, 24, //tamanho do recorte na sprite
+        35, 35, //tamanho do recorte na sprite
         10, 50,
         33, 24,
     );
